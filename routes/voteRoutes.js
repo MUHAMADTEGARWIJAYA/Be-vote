ad74733647d28c89c5ebde487ca34bc9eb5d61cf
tegarmuhamad922@gmail.com <tegarmuhamad922@gmail.com>
2024-12-03 15:48:15 +0700

    uhuy2

--- routes/voteRoutes.js
+++ routes/voteRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import  vote  from "../controllers/voteController.js";
+import { vote } from "../controllers/voteController.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 const router = express.Router();
 
