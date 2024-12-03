49babe64ea85eb3edae4ea777eb85a10f8257c79
tegarmuhamad922@gmail.com <tegarmuhamad922@gmail.com>
2024-12-03 11:02:59 +0700

    tambah verify token

--- routes/voteRoutes.js
+++ routes/voteRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { vote } from "../controllers/voteController.js";
-
+import { verifyToken } from "../middleware/verifyToken.js";
 const router = express.Router();
 
-router.post("/vote", vote);
+router.post("/vote", verifyToken, vote);
 
 export default router;
